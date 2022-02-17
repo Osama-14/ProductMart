3e38c1677646cf652aefe460a44f13592bd66841
Osama-14 <mohdosama42000@gmail.com>
2022-02-17 15:58:34 +0500

    set routes&added header/footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { Route,BrowserRouter, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Cartpage from './pages/Cartpage';
+import ProductInfo from './pages/ProductInfo';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <BrowserRouter>
+     <Routes>
+    <Route path='/' exact element={ <Homepage />} />
+    <Route path='/login' exact element={ <Login />} />
+    <Route path='/register' exact element={ <Register />} />
+    <Route path='/productinfo' exact element={ <ProductInfo />} />
+    <Route path='/cart' exact element={ <Cartpage />} />
+
+
+     </Routes>
+     </BrowserRouter>
+
+
     </div>
   );
 }
