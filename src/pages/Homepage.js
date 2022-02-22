2377643fd642ab9664834f31ee54a63a7995e367
Osama-14 <mohdosama42000@gmail.com>
2022-02-22 12:28:42 +0500

    setting up database

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { collection, addDoc } from "firebase/firestore"; 
+import fireDB from '../fireConfig';
+
 
 function Homepage() {
+
+  async function addData() {
+
+   await addDoc(collection(fireDB, "users"), {name:'osama', age: 25} )
+
+  }
   return (
    <Layout>
        <h1>Home</h1>
+
+       <button onClick={addData}>ADD DATA TO FIREBASE</button>
    </Layout>
   )
 }
