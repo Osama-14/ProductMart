5a34d2a30ee3a8b57e80cc2157eaae479f62529a
Osama-14 <mohdosama42000@gmail.com>
2022-02-24 21:56:50 +0500

    added states

---
+++ src/Products.js
@@ -0,0 +1,18 @@
+export const fireProducts = [{
+    "price": 234,
+
+},
+{
+    "price": 2534,
+
+},
+{
+    "price": 2534,
+
+},
+{
+    "price": 23534,
+
+},
+
+]
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,26 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import Layout from "../components/Layout";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import fireDB from "../fireConfig";
 import { fireProducts } from "../Products";
 
 function Homepage() {
-  async function addData() {
-    try {
-      await addDoc(collection(fireDB, "users"), { name: "mubeen", age: 22 });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+
+  const [products, setProducts] = useState([])
+  
 
   async function getData() {
     try {
-      const users = await getDocs(collection(fireDB, "users"), {
-        name: "mubeen",
-        age: 22,
-      });
+      const users = await getDocs(collection(fireDB, "products"));
 
-      const usersArray = [];
+      const productsArray = [];
 
       users.forEach((doc) => {
         const obj = {
@@ -28,31 +21,22 @@ function Homepage() {
           ...doc.data(),
         };
 
-        usersArray.push(obj);
+        productsArray.push(obj);
       });
-      console.log(usersArray);
+
+      setProducts(productsArray)
     } catch (error) {
       console.log(error);
     }
   }
 
-  function addProductData() {
-    fireProducts.map(async (product) => {
-      try {
-        await addDoc(collection(fireDB, "products"), product);
-      } catch (error) {
-        console.log(error);
-      }
-    });
-  }
+
 
   return (
     <Layout>
       <h1>Home</h1>
 
-      <button onClick={addData}>ADD DATA TO FIREBASE</button>
-      <button onClick={getData}>GET DATA TO FIREBASE</button>
-      <button onClick={addProductData}>Product TO FIREBASE</button>
+      
     </Layout>
   );
 }
