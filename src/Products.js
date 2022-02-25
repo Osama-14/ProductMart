34a6e32bccf3911bafff061e55a40a9a1606e27f
Osama-14 <mohdosama42000@gmail.com>
2022-02-25 23:44:18 +0500

    products done

--- src/Products.js
+++ src/Products.js
@@ -1,33 +1,81 @@
 export const FireProducts = [
   {
-    price: 234,
-    discription: " Apple IOS phone",
-    name: "Iphone",
-    category: " osv",
+    price: 120000,
+    discription: " 8GB RAM , 256GB STORAGE 64 PIXELS CAMERA",
+    name: "Vivo Y20 PRO",
+    category: " SmartPhone",
     imageURL:
-      "https://images.unsplash.com/photo-1567581935884-3349723552ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW9iaWxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+      "https://fdn2.gsmarena.com/vv/pics/vivo/vivo-y20-2021-2.jpg"
   },
   {
-    price: 2214,
-    discription: " sam",
-    name: "samsung",
-    category: " afsc",
+    price: 160000,
+    discription: "4GB RAM , 256GB STORAGE 20 PIXELS CAMERA",
+    name: "SAMSUNG ULTRA PRO",
+    category: " SmartPhone",
     imageURL:
-      "https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Samsung-Galaxy-S20-Family-CC?wid=517&hei=291&fit=crop"
+      "https://www.corning.com/microsites/csm/gorillaglass/Samsung/CGG_Samsung_galaxys21_ultra_phantom.jpg"
   },
   {
-    price: 25264,
-    discription: " best",
-    name: "vivo",
-    category: " elec",
-    imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEaVay23JB6hrfPgw8S2ZsNMEB0qIWxgdkbL1_q1lUieMk9dSxaSFQr_XutTkibCw6-Og&usqp=CAU"
+    price: 400000,
+    discription: "8GB RAM , 256GB STORAGE 64 PIXELS CAMERA",
+    name: "Huawei P20",
+    category: " SmartPhone",
+    imageURL: "https://cdn.dxomark.com/wp-content/uploads/medias/post-88973/featured_huaweip50pro.jpg"
   },
   {
-    price: 2114,
-    discription: " prad",
-    name: "Ip",
-    category: " gagts",
+    price: 100000,
+    discription: "6GB RAM , 256GB STORAGE 64 PIXELS CAMERA",
+    name: "Apple Iphone 13 ULTRA PRO MAX",
+    category: " SmartPhone",
     imageURL:
-     "https://images.hindustantimes.com/img/2021/06/08/1600x900/IMG_5223_1623145084295_1623145098953.jpg"
+     "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-2.jpg"
+  },
+  {
+    price: 26000,
+    discription: "handsome looking smartWatch,heart breath,running",
+    name: "Samsung SmartWatch P21",
+    category: "watches",
+    imageURL:
+     "https://www.kindpng.com/picc/m/87-877607_galaxy-watch-active-image-samsung-galaxy-watch-active.png"
+  },
+  {
+    price: 40000,
+    discription: "handsome looking smartWatch,heart breath,running ",
+    name: "Apple SmartWatch",
+    category: "watches",
+    imageURL:
+     "https://www.kindpng.com/picc/m/381-3819596_apple-watch-gold-aluminum-transparent-background-apple-watch.png"
+  },
+  {
+    price: 500,
+    discription: "100% pure cotton used",
+    name: "Branded Levis t-Shirt",
+    category: "shirts",
+    imageURL:
+     "https://freepngimg.com/thumb/tshirt/10-2-t-shirt-png-image.png"
+  },
+  {
+    price: 4000,
+    discription: "Brand new blutooth headphone",
+    name: "JBL j3100",
+    category: " headphones",
+    imageURL:
+     "https://www.pngitem.com/pimgs/m/302-3025265_headset-bluetooth-jbl-mini-hd-png-download.png"
+  },
+  {
+    price: 3000,
+    discription: " branded headphone with high quality sound system",
+    name: "Audionic Headphone au3200",
+    category: "headphones",
+    imageURL:
+     "https://cdn.shopify.com/s/files/1/0506/6788/0604/products/B-999BLACK_5f559e60-5cb7-4afc-b823-e5cce88d4e21_1024x1024.jpg?v=1613128224"
+  },
+  {
+    price: 200,
+    discription: "digital water proof",
+    name: "Digital watch",
+    category: " watches",
+    imageURL:
+     "https://static-01.daraz.pk/p/013972c7fc0667227723e321a00fa428.jpg"
   },
 ];
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,14 +3,11 @@ import Layout from "../components/Layout";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import fireDB from "../fireConfig";
 import { FireProducts } from "../Products";
-function Homepage({ ...props }) {
-  const { imageURL } = FireProducts;
-
+function Homepage() {
   const [products, setProducts] = useState(FireProducts);
 
   useEffect(() => {
-
-      getData();
+    getData();
   }, []);
 
   async function getData() {
@@ -47,13 +44,26 @@ function Homepage({ ...props }) {
 
             return (
               <div key={ind} className="col-md-4">
-                <div className="m-2 p-1">
-                  <p>{product.name}</p>
-                  <img
-                    src={product.imageURL}
-                    alt=""
-                    className="product-img"
-                  />
+                <div className="m-2 p-1 product position-relative">
+                  <div className="product-content">
+                    <p>{product.name}</p>
+                    <div className="text-center">
+                      <img
+                        src={product.imageURL}
+                        alt=""
+                        className="product-img"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="product-actions">
+                    <h2>{product.price} Rs/</h2>
+                    <div className="d-flex">
+                      <button className="mx-2">Add To Cart</button>
+                      <button>View</button>
+
+                    </div>
+                  </div>
                 </div>
               </div>
             );
